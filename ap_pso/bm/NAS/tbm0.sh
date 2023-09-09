6d1b96fa82799558ffc5b66d98060ce13119f16a
Morridin <paulza@gmx.de>
2023-09-09 14:32:20 +0200

    Removed formatting.

--- ap_pso/bm/NAS/tbm0.sh
+++ ap_pso/bm/NAS/tbm0.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH --partition       = gpu_4_a100
-#SBATCH --gres            = gpu:4
-#SBATCH --nodes           = 4
-#SBATCH --ntasks-per-node = 4
-#SBATCH --cpus-per-task   = 16
-#SBATCH --time            = 16:00:00
-#SBATCH --mem             = 510000
-#SBATCH --output          = "/pfs/work7/workspace/scratch/pa1164-propulate_bm_1/async-parallel-pso/ap_pso/bm/NAS/NAS.out"
-#SBATCH --job-name        = "NAS"
-#SBATCH --mail-type       = all
-#SBATCH --mail-user       = pa1164@partner.kit.edu
+#SBATCH --partition=gpu_4_a100
+#SBATCH --gres=gpu:4
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=16
+#SBATCH --time=16:00:00
+#SBATCH --mem=510000
+#SBATCH --output="/pfs/work7/workspace/scratch/pa1164-propulate_bm_1/async-parallel-pso/ap_pso/bm/NAS/NAS.out"
+#SBATCH --job-name="NAS"
+#SBATCH --mail-type=all
+#SBATCH --mail-user=pa1164@partner.kit.edu
 
 cd \$(ws_find propulate_bm_1)
 ml purge
